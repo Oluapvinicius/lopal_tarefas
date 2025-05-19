c6ad59956bf0fe7c63c105170edc78f518be2a4e
PAULO VINICIUS LIMA DA SILVA <25132413@CFP127ALUNO.LOCAL>
2025-05-19 16:55:55 -0300

    Classe Funcionario criado

--- src/br/dev/paulo/tarefas/Main.java
+++ src/br/dev/paulo/tarefas/Main.java
@@ -1,6 +1,7 @@
 package br.dev.paulo.tarefas;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,34 +9,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.Buffer;
 
+import br.dev.paulo.tarefas.model.Funcionario;
+
 public class Main {
 
 	static String path = "C:\\Users\\25132413\\ProjetoTarefas\\tarefas.txt";
 
 	public static void main(String[] args) {
-
-		lerArquivo();
-//		gravarArquivo();
+		
+		Funcionario funcionario1 = new Funcionario("Maria da silva");//Criação de um Objeto. Que herarquicamente é filho da Classe Object. //Mas no main Criamos um novo construtor
+		Funcionario funcionario2 = new Funcionario();
+		
+		funcionario2.setMatricula(9);
+		funcionario2.setNome("Patricia Gomes");
+		funcionario2.setCargo("Gerente de TI");
+		
+		Funcionario funcionario3 = new Funcionario("Progamador", "Maria");
+		Funcionario funcionario4 = new Funcionario("Progamador", "Pedro", 2);
+		
+		System.out.println(funcionario1);
+		System.out.println(funcionario2);
+		System.out.println(funcionario3);
+		System.out.println(funcionario4);
+		
+		System.out.println("Esse foi o video deixa o like fui..");
 	}
 
+	//	lerArquivo();
+//		gravarArquivo();
+	
+
 	private static void gravarArquivo() {
+	    FileWriter fw = null;
+	    BufferedWriter bw = null; 
 
-		FileWriter fr = null;
-		Buffer bw = null;
+	    try {
+	        fw = new FileWriter(path, true);
+	        bw = new BufferedWriter(fw); 
 
-		try {
-			fw = new FileWriter(path, true);
-			bw = new BufferedReader(fw);
+	        String novalinha = "Isso é uma nova linha!!";
+	        bw.write(novalinha); 
+	        bw.flush();
 
-			String novaLinha = "Isso é uma nova linha!!";
-
-			bw.write(novaLinha);
-
-			bw.flush();
-		} catch (Exception e) {
-			System.out.println(erro.getMessage());
-		}
-
+	    } catch (Exception erro) {
+	        System.out.println(erro.getMessage()); 
+	    } finally {
+	        try {
+	            if (bw != null) bw.close(); // Fecha BufferedWriter
+	            if (fw != null) fw.close(); // Fecha FileWriter
+	        
+	        }
+	        
 	}
 
 	private static void lerArquivo() {
---
+++ src/br/dev/paulo/tarefas/model/Status.java
@@ -0,0 +1,5 @@
+package br.dev.paulo.tarefas.model;
+
+public enum Status {
+
+}
